ef25e9c198fcf7081e5581500f6f8d9f0840cbf9
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2025-08-09 21:39:29 +0100

    feat(backend): add POST endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+# MUCH OF THIS CODE IS PURELY FOR THE TESTING OF THE MVP.
+# SOME PARTICULAR SECTIONS OF TAGGED WITH A "TEMP" MESSAGE.
+# HOWEVER, MUCH OF THE REST WILL CHANGE TOO.
 
-# Temporary in-memory storage for testing MVP
+# TEMP in-memory storage for testing MVP
 # Tracker format: {tracker_name: {id, tracker_name, current, total}}
 trackers = {
     "tracker1": ["1", "The Oxford History of Britain", 582, 751],
@@ -24,3 +27,13 @@ async def get_trackers():
 @app.get("/trackers/{tracker_name}")
 async def get_tracker(tracker_name: str):
     return trackers[tracker_name]
+
+
+# POST Functions
+# TEMP function for testing MVP
+@app.post("/new")
+async def create_tracker(trackername: str, current: int, total: int):
+    global trackers
+    new_id = len(trackers)+1
+    trackers[f"tracker{new_id}"] = [str(new_id), trackername, current, total]
+    return {"message": "Tracker Created!"}
