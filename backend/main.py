dc648aaa0af649eb937e0bc5858ab0db99cff5c6
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2025-08-09 16:27:26 +0100

    fix(backend): store tracker values in a list
    Having the values of each tracker key-value pair stored in a set meant that they were returned in the incorrect order when the tracker was requested from the browser. Storing them in a list will maintain their declared order.

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,8 @@ app = FastAPI()
 # Temporary in-memory storage for testing MVP
 # Tracker format: {tracker_name: {id, tracker_name, current, total}}
 trackers = {
-    "tracker1": {"1", "The Oxford History of Britain", 582, 751}
+    "tracker1": ["1", "The Oxford History of Britain", 582, 751],
+    "tracker2": ["2", "Churchill: Walking with Destiny", 0, 982]
 }
 
 
@@ -14,6 +15,7 @@ async def root():
     return {"message": "Hello, World!"}
 
 
+# GET Functions
 @app.get("/trackers")
 async def get_trackers():
     return trackers
