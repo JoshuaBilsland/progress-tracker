fd7a408722f991dad361fa75e06ba9d45a2d1015
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2025-09-11 22:30:37 +0100

    feat(gui): add delete tracker buttons
    Refactored tracker UI generation into reusable subroutine to allow for page refresh following CRUD actions.

--- gui-frontend/main.py
+++ gui-frontend/main.py
@@ -2,7 +2,7 @@ import os
 import requests
 from dotenv import load_dotenv
 from PyQt6.QtCore import Qt, QSize
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel, QProgressBar, QSizePolicy, QSpacerItem
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel, QProgressBar, QSizePolicy, QSpacerItem, QPushButton
 
 
 class Main(QMainWindow):
@@ -12,25 +12,41 @@ class Main(QMainWindow):
         self.setWindowTitle("Progress Tracker App")
         self.setMinimumSize(QSize(500, 500))
 
-        main_page = QWidget()
-        main_page_layout = QVBoxLayout()
-        main_page_layout.setContentsMargins(0, 0, 0, 0)
-        main_page_layout.setSpacing(0)
-
-        # Retrieve Trackers From Server
-        trackers = self.get_trackers()
+        self.main_page = QWidget()
+        self.main_page_layout = QVBoxLayout()
+        self.main_page_layout.setContentsMargins(0, 0, 0, 0)
+        self.main_page_layout.setSpacing(0)
 
         # Heading Label
-        label = QLabel("Progress Tracker App")
-        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        label.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
-        main_page_layout.addWidget(label)
+        self.heading_label = QLabel("Progress Tracker App")
+        self.heading_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.heading_label.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        self.main_page_layout.addWidget(self.heading_label)
 
         # Store progress bars by tracker_name/key for individual retrieval
-        trackers_container = QWidget()
-        trackers_layout = QVBoxLayout()
-        trackers_layout.setContentsMargins(40, 40, 40, 40)
-        trackers_layout.setSpacing(20)
+        self.trackers_container = QWidget()
+        self.create_trackers_layout()
+
+        self.trackers_container.setLayout(self.trackers_layout)
+        self.main_page_layout.addWidget(self.trackers_container)
+
+        self.main_page.setLayout(self.main_page_layout)
+        self.setCentralWidget(self.main_page)
+
+    def refresh_trackers(self):
+        container_index = self.delete_trackers_structure()
+        self.create_trackers_layout()
+        self.trackers_container.deleteLater()
+        self.trackers_container = QWidget()
+        self.trackers_container.setLayout(self.trackers_layout)
+        self.main_page_layout.insertWidget(container_index, self.trackers_container)
+        self.centralWidget().update()
+
+    def create_trackers_layout(self):
+        trackers = self.get_trackers()
+        self.trackers_layout = QVBoxLayout()
+        self.trackers_layout.setContentsMargins(40, 40, 40, 40)
+        self.trackers_layout.setSpacing(20)
 
         self.progress_bars = {}
         for tracker_name, tracker_data in trackers.items():
@@ -39,12 +55,14 @@ class Main(QMainWindow):
             tracker_layout.setContentsMargins(0, 0, 0, 0)
             tracker_layout.setSpacing(15)
 
+            # Tracker Label
             tracker_label = QLabel(f"{tracker_data[1]} - {tracker_data[2]}/{tracker_data[3]}")
             tracker_label.setObjectName("tracker-label")
             tracker_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
             tracker_label.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
             tracker_layout.addWidget(tracker_label)
 
+            # Tracker Bar
             progress_bar = QProgressBar(self)
             progress_bar.setMinimum(0)
             progress_bar.setMaximum(tracker_data[3])
@@ -52,22 +70,43 @@ class Main(QMainWindow):
             progress_bar.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
             tracker_layout.addWidget(progress_bar)
 
+            # Tracker Delete Button
+            delete_button = QPushButton("Delete")
+            delete_button.clicked.connect(lambda checked, tn=tracker_name: self.delete_tracker(tn))
+            delete_button.clicked.connect(lambda checked: self.refresh_trackers())
+            tracker_layout.addWidget(delete_button)
+
             tracker_container.setLayout(tracker_layout)
-            trackers_layout.addWidget(tracker_container)
-            trackers_layout.addSpacerItem(QSpacerItem(20, 20, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding))
+            self.trackers_layout.addWidget(tracker_container)
+            self.trackers_layout.addSpacerItem(QSpacerItem(20, 20, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding))
             self.progress_bars[tracker_name] = progress_bar
-
-        trackers_container.setLayout(trackers_layout)
-        main_page_layout.addWidget(trackers_container)
-
-        main_page.setLayout(main_page_layout)
-        self.setCentralWidget(main_page)
 
     def get_trackers(self):
         backend_ip = os.getenv("BACKEND_IP")
         url = f"http://{backend_ip}:8000/trackers"
         response = requests.get(url)
         response.raise_for_status()
+        return response.json()
+
+    def delete_trackers_structure(self):
+        # Delete existing trackers layout and widgets
+        while self.trackers_layout.count():  # Keeps going until no more widgets in layout
+            child = self.trackers_layout.takeAt(0)
+            if child.widget():
+                child.widget().deleteLater()
+
+        # Find the index of the previous container within the main page layout
+        for i in range(self.main_page_layout.count()):
+            item = self.main_page_layout.itemAt(i)
+            widget = item.widget()
+            if widget == self.trackers_container:
+                index = i
+        return index
+
+    def delete_tracker(self, trackername):
+        backend_ip = os.getenv("BACKEND_IP")
+        url = f"http://{backend_ip}:8000/delete"
+        response = requests.delete(url, params={"trackername": trackername})
         return response.json()
 
 
